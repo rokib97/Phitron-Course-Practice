d43855ec1a317801e91b469efd92a734d86b1c99
rokib97 <hasanrokib1997@gmail.com>
2022-10-21 14:57:42 +0600

    added two problems

---
+++ Conceptual-01/ascii.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+
+
+    char ch;
+    printf("Please Enter A Letter: ");
+    scanf("%c",&ch);
+    printf("%d",ch);
+
+    return 0;
+}
---
+++ Conceptual-01/type_casting.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+
+    int a = 10;
+    // int b = (int)10.55;
+    float c = (float)a;
+
+    // printf("%d\n%d", a, b);
+    printf("%f", c);
+
+    return 0;
+}
