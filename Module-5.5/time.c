784f1d7566adce9d07997ee0ab30e59781c77d5c
rokib97 <hasanrokib1997@gmail.com>
2022-10-30 01:04:50 +0600

    added more solve

---
+++ Module-5.5/time.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+
+    int n, m=24;
+    scanf("%d %d",&n,&m);
+   for(int i = n; i<=m ; i++){
+  
+    // WILL IMPLEMENT LOGIC LATER 
+    //DINNER TIME
+    return 0;
+}
